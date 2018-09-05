91928aabaa0df0f7f6cab106683e4951c80b34d2
Roman Golyshev <anstreth@gmail.com>
2018-09-05 23:38:24 +0300

    add solution for 113A problem and tests from examples

--- src/main/kotlin/ru/hse/spb/main.kt
+++ src/main/kotlin/ru/hse/spb/main.kt
@@ -1,13 +1,70 @@
 package ru.hse.spb
 
-fun getGreeting(): String {
-    val words = mutableListOf<String>()
-    words.add("Hello,")
-    words.add("world!")
+/**
+ * Solution for 113A problem ([link](http://codeforces.com/problemset/problem/113/A)).
+ */
 
-    return words.joinToString(separator = " ")
+private enum class Gender {
+    MALE, FEMALE
+}
+
+private enum class WordType {
+    VERB, NOUN, ADJECTIVE
+}
+
+private data class Word(val type: WordType, val gender: Gender) {
+
+    companion object {
+        fun parseWord(word: String): Word? =
+            when {
+                word.endsWith("lios") -> Word(WordType.ADJECTIVE, Gender.MALE)
+                word.endsWith("liala") -> Word(WordType.ADJECTIVE, Gender.FEMALE)
+
+                word.endsWith("etr") -> Word(WordType.NOUN, Gender.MALE)
+                word.endsWith("etra") -> Word(WordType.NOUN, Gender.FEMALE)
+
+                word.endsWith("initis") -> Word(WordType.VERB, Gender.MALE)
+                word.endsWith("inites") -> Word(WordType.VERB, Gender.FEMALE)
+
+                else -> null
+            }
+    }
+
+}
+
+private fun genderIsSame(validWords: List<Word>): Boolean {
+    val expectedGender = validWords.firstOrNull()?.gender ?: return false
+    return validWords.all { it.gender === expectedGender }
+}
+
+private fun structureIsCorrect(validWords: List<Word>): Boolean {
+    val firstNounIndex = validWords.indexOfFirst { it.type === WordType.NOUN }
+    if (firstNounIndex == -1) return false
+
+    val adjectives = validWords.subList(0, firstNounIndex)
+    val verbs = validWords.subList(firstNounIndex + 1, validWords.size)
+
+    return adjectives.all { it.type === WordType.ADJECTIVE }
+        && verbs.all { it.type === WordType.VERB }
+}
+
+private fun isPhrase(words: List<Word?>): Boolean {
+    val validWords = words.filterNotNull()
+
+    return validWords.size == words.size
+        && genderIsSame(validWords)
+        && structureIsCorrect(validWords)
+}
+
+fun isCorrectSentence(words: List<String>): Boolean {
+    val parsedWords = words.map { Word.parseWord(it) }
+
+    return parsedWords.singleOrNull() != null
+        || isPhrase(parsedWords)
 }
 
 fun main(args: Array<String>) {
-    println(getGreeting())
+    val words = readLine()?.split(' ') ?: return
+
+    println(if (isCorrectSentence(words)) "YES" else "NO")
 }
--- src/test/kotlin/ru/hse/spb/test.kt
+++ src/test/kotlin/ru/hse/spb/test.kt
@@ -1,11 +1,13 @@
 package ru.hse.spb
 
-import org.junit.Assert.assertEquals
+import org.junit.Assert.*
 import org.junit.Test
 
 class TestSource {
     @Test
-    fun testGreeting() {
-        assertEquals("Hello, world!", getGreeting())
+    fun testExamples() {
+        assertTrue(isCorrectSentence("petr".split(' ')))
+        assertFalse(isCorrectSentence("etis atis animatis etis atis amatis".split(' ')))
+        assertTrue(isCorrectSentence("nataliala kataliala vetra feinites".split(' ')))
     }
 }
