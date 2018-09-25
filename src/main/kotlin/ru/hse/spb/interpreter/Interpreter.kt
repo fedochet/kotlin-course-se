9bc1d68f823d812bfca7bec9d90989128925d49c
Roman Golyshev <anstreth@gmail.com>
2018-09-26 01:40:30 +0300

    move from case classes to nullable Int result

--- src/main/kotlin/ru/hse/spb/interpreter/Interpreter.kt
+++ src/main/kotlin/ru/hse/spb/interpreter/Interpreter.kt
@@ -9,7 +9,7 @@ class WrongNumberOfArgsException(val expected: List<String>, val actual: List<Ex
 class FunctionRedeclarationException(val name: String) : RuntimeException()
 
 class Context private constructor(val parent: Context? = null) {
-    private val variables: MutableMap<String, Int> = mutableMapOf()
+    private val variables: MutableMap<String, kotlin.Int> = mutableMapOf()
     private val functions: MutableMap<String, FunctionDeclaration> = mutableMapOf()
 
     fun declareFunction(declaration: FunctionDeclaration) {
@@ -23,18 +23,18 @@ class Context private constructor(val parent: Context? = null) {
             ?: throw VariableNotFound(name)
     }
 
-    fun declareVariable(name: String, initializer: Int) {
+    fun declareVariable(name: String, initializer: kotlin.Int) {
         if (name in variables) throw VariableRedeclarationException(name)
         variables[name] = initializer
     }
 
-    fun getVariable(name: String): Int {
+    fun getVariable(name: String): kotlin.Int {
         return variables[name]
             ?: parent?.getVariable(name)
             ?: throw VariableNotFound(name)
     }
 
-    fun setVariable(name: String, value: Int) {
+    fun setVariable(name: String, value: kotlin.Int) {
         when {
             name in variables -> variables[name] = value
             parent != null -> parent.setVariable(name, value)
@@ -49,17 +49,9 @@ class Context private constructor(val parent: Context? = null) {
     }
 }
 
-sealed class BlockExecutionResult
-data class ReturnResult(val value: Int) : BlockExecutionResult()
-object NoReturnResult : BlockExecutionResult()
+fun evalBlock(block: Block, ctx: Context): Int = executeBlock(block, ctx) ?: 0
 
-fun evalBlock(block: Block, ctx: Context): Int {
-    val result = executeBlock(block, ctx)
-
-    return if (result is ReturnResult) result.value else 0
-}
-
-fun call(function: FunctionDeclaration, args: List<Expression>, ctx: Context): Int {
+fun call(function: FunctionDeclaration, args: List<Expression>, ctx: Context): kotlin.Int {
     if (function.args.size != args.size) throw WrongNumberOfArgsException(function.args, args)
 
     val functionCtx = ctx.derive()
@@ -70,48 +62,49 @@ fun call(function: FunctionDeclaration, args: List<Expression>, ctx: Context): I
     return evalBlock(function.body, functionCtx)
 }
 
-private fun executeBlock(block: Block, ctx: Context): BlockExecutionResult {
+private fun executeBlock(block: Block, ctx: Context): Int? {
     for (statement in block.statements) {
-        val result = executeStatement(statement, ctx)
-        if (result is ReturnResult) return result
+        executeStatement(statement, ctx)?.let { return it }
     }
 
-    return NoReturnResult
+    return null
 }
 
-private fun executeStatement(statement: Statement, ctx: Context): BlockExecutionResult =
+private fun executeStatement(statement: Statement, ctx: Context): Int? {
     when (statement) {
-        is Return -> ReturnResult(evalExpression(statement.value, ctx))
+        is Return -> return evalExpression(statement.value, ctx)
 
         is If -> {
             val condition = evalExpression(statement.condition, ctx)
-            if (condition != 0) {
+            return if (condition != 0) {
                 executeBlock(statement.thenBlock, ctx.derive())
             } else {
-                statement.elseBlock?.let { executeBlock(it, ctx.derive()) } ?: NoReturnResult
+                statement.elseBlock?.let { executeBlock(it, ctx.derive()) }
             }
         }
 
-        is FunctionDeclaration -> {
-            ctx.declareFunction(statement)
-            NoReturnResult
-        }
+        is FunctionDeclaration -> ctx.declareFunction(statement)
 
         is VarDeclaration -> {
             val initializer = statement.initializer?.let { evalExpression(it, ctx) } ?: 0
             ctx.declareVariable(statement.name, initializer)
-            NoReturnResult
         }
 
-        is Expression -> {
-            evalExpression(statement, ctx)
-            NoReturnResult
-        }
+        is Expression -> evalExpression(statement, ctx)
 
-        else -> TODO()
+        is Assignment -> ctx.setVariable(statement.name, evalExpression(statement.value, ctx))
+
+        is While -> {
+            while (evalExpression(statement.condition, ctx).toBool()) {
+                executeBlock(statement.body, ctx.derive())?.let { return it }
+            }
+        }
     }
 
-fun evalExpression(expr: Expression, ctx: Context): Int {
+    return null
+}
+
+fun evalExpression(expr: Expression, ctx: Context): kotlin.Int {
     return when (expr) {
         is Literal -> expr.value
         is Ident -> ctx.getVariable(expr.name)
@@ -120,7 +113,7 @@ fun evalExpression(expr: Expression, ctx: Context): Int {
     }
 }
 
-private fun evalBinop(left: Int, op: Operation, right: Int): Int =
+private fun evalBinop(left: kotlin.Int, op: Operation, right: kotlin.Int): kotlin.Int =
     when (op) {
         MULTIPLY -> left * right
         DIVIDE -> left / right
@@ -137,5 +130,5 @@ private fun evalBinop(left: Int, op: Operation, right: Int): Int =
         NEQ -> (left != right).toInt()
     }
 
-private fun Int.toBool() = this != 0
+private fun kotlin.Int.toBool() = this != 0
 private fun Boolean.toInt() = if (this) 1 else 0
