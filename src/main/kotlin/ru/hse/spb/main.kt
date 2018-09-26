b5d780a2f0762e3bea17437f1650dd6aada0ef32
Roman Golyshev <anstreth@gmail.com>
2018-09-26 09:21:14 +0300

    add main

--- src/main/kotlin/ru/hse/spb/main.kt
+++ src/main/kotlin/ru/hse/spb/main.kt
@@ -2,14 +2,17 @@ package ru.hse.spb
 
 import org.antlr.v4.runtime.BufferedTokenStream
 import org.antlr.v4.runtime.CharStreams
-import ru.hse.spb.parser.ExpressionParser
+import ru.hse.spb.interpreter.Context
+import ru.hse.spb.interpreter.evalBlock
+import ru.hse.spb.parser.BlockParser
 import ru.hse.spb.parser.FunLangLexer
 import ru.hse.spb.parser.FunLangParser
 
 fun main(args: Array<String>) {
+    val lexer = FunLangLexer(CharStreams.fromFileName(args[0]))
+    val parser = FunLangParser(BufferedTokenStream(lexer))
 
-    val funLangLexer = FunLangLexer(CharStreams.fromString("1+2"))
-    val parse = ExpressionParser.visit(FunLangParser(BufferedTokenStream(funLangLexer)).parse())
+    val block = BlockParser.visit(parser.parse())
 
-    println(parse)
+    evalBlock(block, Context.empty())
 }
