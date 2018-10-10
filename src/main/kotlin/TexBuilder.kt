854afd970b59f5be23e889917b4974f4a1558d95
Roman Golyshev <anstreth@gmail.com>
2018-10-10 23:38:07 +0300

    refactor, make common base for all tex tags

--- src/main/kotlin/TexBuilder.kt
+++ src/main/kotlin/TexBuilder.kt
@@ -1,6 +1,3 @@
-import java.io.ByteArrayOutputStream
-import java.io.OutputStream
-
 val LINE_SEPARATOR = System.getProperty("line.separator");
 
 typealias Param = Pair<String, String>
@@ -36,6 +33,23 @@ abstract class TexTag : Renderable {
         content.add(TexList.enumerated().apply(action).render())
     }
 
+    operator fun String.unaryPlus() {
+        content.add(this)
+    }
+
+    // todo forbid to use frames inside frames
+    fun frame(frameTitle: String, vararg params: Param, builder: BeamerFrame.() -> Unit) {
+        TODO()
+    }
+
+    fun equation(builder: EquationEnv.() -> Unit) {
+        content.add(EquationEnv().apply(builder).render())
+    }
+
+    fun customTag(name: String, vararg params: Param, builder: CustomTag.() -> Unit) {
+        content.add(CustomTag(name, params).apply(builder).render())
+    }
+
     override fun render(): String {
         return buildString {
             appendln("\\begin{$name}")
@@ -63,47 +77,16 @@ class BeamerFrame : TexTag() {
     override val name: String = "frame"
 }
 
-class GenericTag {
-    operator fun String.unaryPlus() {
-        TODO()
-    }
-}
+class CustomTag(override val name: String, params: Array<out Param>) : TexTag()
 
-class TexBuilder : Renderable {
+class TexBuilder : TexTag() {
+    override val name = "document"
+
     private var documentClass: String? = null
-
     private val packages: MutableList<TexPackage> = mutableListOf()
-    private val content: MutableList<String> = mutableListOf()
 
-    override fun render(): String = ByteArrayOutputStream().also { toOutputStream(it) }.toString()
-
-    fun documentClass(name: String) {
-        documentClass = name
-    }
-
-    fun usepackage(name: String, vararg options: String) {
-        packages.add(TexPackage(name, options.asList()))
-    }
-
-    operator fun String.unaryPlus() {
-        content.add(this)
-    }
-
-    fun itemize(action: TexList.() -> Unit) {
-        content.add(TexList.itemized().apply(action).render())
-    }
-
-    fun enumerate(action: TexList.() -> Unit) {
-        content.add(TexList.enumerated().apply(action).render())
-    }
-
-    // todo forbid to use frames inside frames
-    fun frame(frameTitle: String, vararg params: Param, builder: BeamerFrame.() -> Unit) {
-        TODO()
-    }
-
-    fun toOutputStream(stream: OutputStream) {
-        stream.writer().run {
+    override fun render(): String {
+        return buildString {
             documentClass?.let {
                 appendln("\\documentclass{$it}")
             }
@@ -112,21 +95,17 @@ class TexBuilder : Renderable {
                 appendln(pkg.render())
             }
 
-            appendln("\\begin{document}")
-
-            for (entry in content) {
-                appendln(entry)
-            }
-
-            append("\\end{document}")
-        }.flush()
+            append(super.render())
+        }
     }
 
-    fun equation(builder: EquationEnv.() -> Unit) {
-        content.add(EquationEnv().apply(builder).render())
+    fun documentClass(name: String) {
+        documentClass = name
     }
 
-    fun customTag(name: String, vararg params: Param, builder: GenericTag.() -> Unit) = GenericTag().apply(builder)
+    fun usepackage(name: String, vararg options: String) {
+        packages.add(TexPackage(name, options.asList()))
+    }
 
     private fun TexPackage.render(): String {
         val formattedOptions = if (options.isNotEmpty()) options.joinToString(",", "[", "]") else ""
@@ -151,7 +130,7 @@ fun document(builder: TexBuilder.() -> Unit) = TexBuilder().apply(builder)
 fun main(args: Array<String>) {
     val rows = listOf("item 1", "item 2")
 
-    document {
+    val document = document {
         documentClass("beamer")
         usepackage("babel", "russian" /* varargs */)
         frame("frametitle", "arg1" to "arg2") {
@@ -169,5 +148,7 @@ fun main(args: Array<String>) {
             """
             }
         }
-    }.toOutputStream(System.out)
+    }
+
+    println(document.render())
 }
